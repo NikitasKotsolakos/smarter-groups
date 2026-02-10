17ba766366a867f439909f25f337904286bebeb6
Nikitas Kotsolakos <kotsolakos.nikitas@gmail.com>
2026-02-10 18:05:55 +0100

    Apply suggestions from code reviewRemove nullable from foreign keys

--- database/migrations/2024_09_07_065128_create_classrooms_table.php
+++ database/migrations/2024_09_07_065128_create_classrooms_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->timestamps();
             $table->string('name');
-            $table->foreignId('workshop_id')->nullable()->constrained()->cascadeOnDelete();
+            $table->foreignId('workshop_id')->constrained()->cascadeOnDelete();
         });
     }
 
